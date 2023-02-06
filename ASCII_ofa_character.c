e492cd7b98c63098b0bc7adf249d1fd19cce1e6f
Joseph Kakai <josephngalu96@gmail.com>
2023-02-05 22:50:16 -0800

    ASCII value of a character

---
+++ ASCII_ofa_character.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the ASCII value of a character
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	printf("Please input a character to print it ASCII value");
+	scanf("%c", &a);
+	printf("The ASCII value of your input will be: %d", a);
+	return (0);
+}
